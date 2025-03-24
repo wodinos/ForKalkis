850c9737001a5d22c886840f39cb30f719be9389
juzmule <stian.lundberg96@gmail.com>
2025-03-24 15:06:20 +0100

    Mobile changes

--- src/App.jsx
+++ src/App.jsx
@@ -186,10 +186,10 @@ export default function App() {
   );
 
   return (
-    <div className="max-w-2xl mx-auto p-4 my-6 bg-white dark:bg-gray-900 text-black dark:text-white rounded shadow-md">
+    <div className="max-w-2xl mx-auto p-4 my-0 sm:my-6 bg-white dark:bg-gray-900 text-black dark:text-white rounded shadow-md">
       {/* Header */}
-      <div className="flex items-center justify-between mb-6">
-        <h1 className="text-3xl font-bold">{t.title}</h1>
+      <div className="flex-row place-items-center items-center justify-between mb-6 sm:flex">
+        <h1 className="text-lg font-bold sm:text-3xl">{t.title}</h1>
         <div className="flex gap-2">
           <button
             onClick={toggleLanguage}
